35c575141b9bd4178caf85a85340e85eb97e8958
Carlos Andrés Vargas Escobar <carlosa.vargase@gmail.com>
2025-10-24 00:10:49 -0500

    add url to env variables, add .env to .gitignore

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL
+
 export default axios.create({
-  baseURL: "http://localhost:4000/api", 
+  baseURL: `${API_URL}/api`, 
   headers: {
     "Content-Type": "application/json",
   },
